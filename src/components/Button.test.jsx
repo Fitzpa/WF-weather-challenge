25e6ff9d8e83bc96ebd9ecf3048a92dfdd20b923
Louie Fitzpatrick <fitz4492@gmail.com>
2022-10-05 19:55:38 -0500

    tested url for button component

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -1,17 +1,31 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { it, describe, expect } from "vitest";
 /**
  * @vitest-environment jsdom
  */
 import Button from "./Button";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { it, describe } from "vitest";
+
+const MockRouter = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
 
 describe("Button", () => {
   it("should render a button with a name and type", () => {
     const buttonName = "Submit";
 
     render(<Button name={buttonName} buttonType="submit" />);
+    expect(screen.getByText(buttonName)).toBeInTheDocument();
+  });
+  it("should render a Link with a name and url", () => {
+    const buttonName = "I am a link";
+    const url = "/five-day-weather-forecast";
+
+    render(
+      <MockRouter>
+        <Button name={buttonName} url={url} />
+      </MockRouter>
+    );
     expect(screen.getByText(buttonName)).toBeInTheDocument();
   });
 });
